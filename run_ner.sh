94d937ea0f83b38f0e194e0aa24b9098ee6c1907
zerohd4869 <1228868719@qq.com>
2020-07-17 11:07:15 +0800

    add slk-ner

--- run_ner.sh
+++ run_ner.sh
@@ -1,19 +1,18 @@
 export CUDA_VISIBLE_DEVICES=0
 python run_ner.py
-    --data_dir ./data/resume/ \
+    --data_dir ./data/weibo/ \
     --model_type bert \
     --model_name_or_path bert-base-chinese \
-    --output_dir ./pretrain_bert \
-    --cache_dir /data/project/TEMP_transformers \
+    --output_dir ./weibo_slk_model \
+    --cache_dir ./TEMP_transformers \
     --max_seq_length 256 \
     --do_train \
-    --do_eval \
     --evaluate_during_training \
-    --per_gpu_train_batch_size 8 \
-    --per_gpu_eval_batch_size 8 \
+    --per_gpu_train_batch_size 32 \
+    --per_gpu_eval_batch_size 32 \
     --learning_rate 5e-5 \
-    --num_train_epochs 20 \
-    --logging_steps 200 
+    --num_train_epochs 10 \
+    --logging_steps 10
 
 
 
